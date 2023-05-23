9f7c7f061d66727adff719a04ff054829997da61
Jasmin Pöysti <jasmin.poysti@gmail.com>
2023-05-23 13:31:43 +0300

    first commit

---
+++ models/contacts.js
@@ -0,0 +1,12 @@
+const mongoose = require ("mongoose");
+
+let Schema = mongoose.Schema({
+    "firstname":String,
+    "lastname":{"type":String,"index":true},
+    "email":String,
+    "phone":String
+
+
+})
+
+module.exports = mongoose.model("Contact",Schema);
